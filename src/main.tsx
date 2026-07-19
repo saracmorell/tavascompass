2f7b12c6240a035389bc6d404ea5d5e078f7b42b
Sara C. Morell <support@tavasworld.com>
2026-07-19 09:53:38 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,16 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>BrowserRouter>
-  </React.StrictMode>React.StrictMode>
-  );</React.StrictMode>
